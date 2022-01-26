0640d901e2b669fa40b44c55196bacd9b8a782c5
AnnelyKalmus <annelykalmus@icloud.com>
2022-01-26 14:33:44 +0200

    Added REST controller

---
+++ quiz/src/main/java/com/example/quiz/dto/HealthcheckDto.java
@@ -0,0 +1,10 @@
+package com.example.quiz.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class HealthcheckDto {
+    private boolean status;
+    private String message;
+}
